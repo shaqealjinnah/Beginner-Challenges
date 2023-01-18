2c834fe443964961ace50dc1f9808234309fc61c
shaqealjinnah <j.shaqeal@gmail.com>
2023-01-18 22:43:32 +1100

    Advanced Challenges Part 1

--- app.js
+++ app.js
@@ -266,3 +266,47 @@ function convertToBoolean(arr) {
 }
 
 console.log(convertToBoolean([500, 0, "David", "", []]))
+
+
+// Advanced Challenges
+
+// Q1. Given a rating, display a star (*) for each full rating and a full-stop (.) for each half rating.
+
+function showRating(rating) {
+    let ratings = "";
+    for (i = 0; i < Math.floor(rating); i++) {
+        ratings += "*"
+        if (i !== Math.floor(rating) - 1) {
+            ratings += " "
+        }
+    }
+    if (!Number.isInteger(rating)) {
+        ratings += " ."
+    }
+    return ratings;
+}
+
+console.log(showRating(5));
+
+
+// Q2. Given an array of numbers, return the prices sorted by low to high.
+
+function sortLowToHigh(numbers) {
+    return numbers.sort((a, b) => a - b)
+}
+
+console.log(sortLowToHigh([20, 40, 10, 30, 50, 10]))
+
+
+// Q3. Given an array of objects, return the prices sorted by high to low.
+
+function sortHighToLow(numbers) {
+    return numbers.sort((a, b) => b.price - a.price)
+}
+
+console.log(sortHighToLow([
+    {id: 1, price: 50},
+    {id: 2, price: 30},
+    {id: 3, price: 60},
+    {id: 4, price: 10},
+]))
